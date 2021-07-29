0d2e60dfd910d0c00447f52b748f0f03fa732a37
anmol1104 <anmpahwa@ucdavis.edu>
2021-07-29 15:08:18 +0530

    First build of Network.jl

---
+++ src/Network.jl
@@ -0,0 +1,50 @@
+using HTTP
+using DataFrames
+include("XMLiterate.jl")
+
+"""
+    network(;apikey, bbox)
+
+Returns shape for every street of the network within the bounding box.
+
+# Arguments
+apikey:String   : API key to access HERE
+bbox:String     : Bounding box defined as `"lat₁,long₁;lat₂,long₂"`
+
+For more information visit: https://developer.here.com/documentation/traffic/dev_guide/topics/guide.html
+
+Example request: https://developer.here.com/documentation/traffic/dev_guide/topics_v6.1/example-flow-bounding-box.html
+"""
+function network(;apikey, bbox)
+    baseurl = "https://traffic.ls.hereapi.com"
+    path = "traffic/6.2"
+    resource = "flow.xml"
+    url = "$baseurl/$path/$resource?apiKey=$apikey&bbox=$bbox&responseattributes=shape"
+    df = DataFrame(LID = Int64[], NAME = String[], LENGTH = Float64[], FFS = Float64[], SHAPE = String[])
+
+    # Building network
+    r = HTTP.request(:GET, url)
+    f = parsexml(String(r.body))
+    rootNode = root(f)
+    for node in iterate(rootNode)
+        if nodename(node) == "FI"
+            shape = ""
+            for child in eachelement(node)
+                # Fetching name and length
+                if child == firstelement(node)
+                    push!(df[!, :LID], length(df[!, :LID]) + 1)
+                    push!(df[!, :NAME], child["DE"])
+                    push!(df[!, :LENGTH], parse(Float64, child["LE"]))
+                # Fetching free flow speed 
+                elseif child == lastelement(node)
+                    push!(df[!, :FFS], parse(Float64, child["FF"]))
+                # Fetching link shape (Note: Link shape is of the same format as the bbox)
+                else
+                    shape *= replace(nodecontent(child), " " => ";")
+                end
+            end
+            push!(df[!, :SHAPE], shape)
+        end
+    end
+    return df
+end
