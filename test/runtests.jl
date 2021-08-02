7320c260e27854644a09a46358da022e6e3c9093
anmol1104 <anmpahwa@ucdavis.edu>
2021-08-02 16:50:57 +0530

    Enabled keys for local and CI testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,29 @@ using HERE
 using Test
 using Revise
 
+function loadkey()
+    if "key" in keys(ENV)
+        return ENV[key]
+    else
+        open("test\\keys") do f
+            return strip(readline(f))
+        end
+    end
+end
+
+testkey = loadkey()
 @testset "Traffic API" begin
-    df = flow(apikey=key, bbox="39.8485715,-86.0969867;39.8358934,-86.0757964")
+    df = flow(apikey=testkey, bbox="39.8485715,-86.0969867;39.8358934,-86.0757964")
     @test !isempty(df)
 end
 
 @testset "Weather API" begin
-    df = weather(;apikey=key, info="weather observation", loc=(name = "Jaipur",))
+    df = weather(;apikey=testkey, info="weather observation", loc=(name = "Jaipur",))
     @test !isempty(df)
 
-    df = weather(;apikey=key, info="weather forecast", loc=(name = "Jaipur",))
+    df = weather(;apikey=testkey, info="weather forecast", loc=(name = "Jaipur",))
     @test !isempty(df)
 
-    df = weather(;apikey=key, info="astronomy forecast", loc=(name = "Jaipur",))
+    df = weather(;apikey=testkey, info="astronomy forecast", loc=(name = "Jaipur",))
     @test !isempty(df)
 end
