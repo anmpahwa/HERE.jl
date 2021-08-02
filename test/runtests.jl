ec958ca7ecdfe4493aecb43f7e087313faff220f
anmol1104 <anmpahwa@ucdavis.edu>
2021-08-02 13:57:16 +0530

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using HERE
 using Test
+using Revise
+using DataFrames
+include("keys.jl")
+
+#=
+# Traffic API
+testBox = "39.8485715,-86.0969867;39.8358934,-86.0757964"
+flow(apikey=key, bbox=testBox)
+=#
+
+# Weather API
+
 
 @testset "HERE.jl" begin
-    # Write your tests here.
+    df = flow(apikey=key, bbox="39.8485715,-86.0969867;39.8358934,-86.0757964")
+    @test !isempty(df)
+
+    df = weather(;apikey=key, info="weather observation", loc=(name = "Jaipur",))
+    @test !isempty(df)
+
+    df = weather(;apikey=key, info="weather forecast", loc=(name = "Jaipur",))
+    @test !isempty(df)
+
+    df = weather(;apikey=key, info="astronomy forecast", loc=(name = "Jaipur",))
+    @test !isempty(df)
 end
