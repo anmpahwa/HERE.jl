3c9b2223d44c3ca664252d04301e4394476da4d7
anmol1104 <anmpahwa@ucdavis.edu>
2021-08-02 20:23:55 +0530

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,16 @@ using Test
 using Revise
 
 function loadkey()
-    if "key" in keys(ENV)
+    if "key" in keys(ENV)               # For Travis CI testing
         return ENV["key"]
     else
-        open("test\\keys") do f
+        open("test\\keys") do f         # For local testing
             return strip(readline(f))
         end
     end
 end
+
+testkey = loadkey()
 
 @testset "Traffic API" begin
     df = flow(apikey=testkey, bbox="39.8485715,-86.0969867;39.8358934,-86.0757964")
@@ -26,4 +28,4 @@ end
 
     df = weather(;apikey=testkey, info="astronomy forecast", loc=(name = "Jaipur",))
     @test !isempty(df)
-end=#
+end
