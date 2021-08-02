0115661d6e6de56a9407e675927d94086ae4a1b1
anmol1104 <anmpahwa@ucdavis.edu>
2021-08-02 20:18:56 +0530

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 using HERE
 using Test
-#using Revise
+using Revise
 
-#=
 function loadkey()
     if "key" in keys(ENV)
         return ENV["key"]
@@ -12,13 +11,8 @@ function loadkey()
         end
     end
 end
-=#
 
-testkey = ENV["key"]
-@testset "dummy test" begin
-    @test typeof(testkey) == String
-end
-#=@testset "Traffic API" begin
+@testset "Traffic API" begin
     df = flow(apikey=testkey, bbox="39.8485715,-86.0969867;39.8358934,-86.0757964")
     @test !isempty(df)
 end
